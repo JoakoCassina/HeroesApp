2b963b00559bb6f73127f72aa8cfc1ed34b5d932
Joaquin Cassina <jcassina@ams.red>
2024-07-25 07:35:24 -0300

    pagina web de heroes

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule
+  ],
+  providers: [ provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync() ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
